4a63c4198ba5b4c0a490fb49b25544c523d68f82
andferale <andferale@alum.us.es>
2018-07-19 19:52:10 +0200

    #41 Added category filter to posts search

--- app/src/main/java/com/congresy/congresy/ShowAllPostsActivity.java
+++ app/src/main/java/com/congresy/congresy/ShowAllPostsActivity.java
@@ -1,7 +1,9 @@
 package com.congresy.congresy;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -32,7 +34,10 @@ public class ShowAllPostsActivity extends BaseActivity implements SearchView.OnQ
     ListView list;
     SearchView search;
 
+    ListView lv;
+
     Button myPosts;
+    Button filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,13 +49,41 @@ public class ShowAllPostsActivity extends BaseActivity implements SearchView.OnQ
 
         search = findViewById(R.id.search);
         myPosts = findViewById(R.id.myPosts);
+        filter = findViewById(R.id.filter);
 
         loadMostVotedPosts();
 
-        List<Post> aux = new ArrayList<>(posts);
+        try {
+            Intent myIntent = getIntent();
+            String theme_filter = myIntent.getExtras().get("theme_filter").toString();
 
-        ListView lv = findViewById(R.id.listViewAll);
-        adapter = new PostListSearchAdapter(this, posts, aux);
+            List<Post> filteredPosts = new ArrayList<>();
+
+            if (!theme_filter.equals("None")){
+                for(Post p : posts){
+                    if(p.getCategory().equals(theme_filter)){
+                        filteredPosts.add(p);
+                    }
+                }
+
+                List<Post> aux = new ArrayList<>(filteredPosts);
+
+                lv = findViewById(R.id.listViewAll);
+                adapter = new PostListSearchAdapter(this, filteredPosts, aux);
+
+            } else {
+                List<Post> aux = new ArrayList<>(posts);
+
+                lv = findViewById(R.id.listViewAll);
+                adapter = new PostListSearchAdapter(this, posts, aux);
+            }
+        } catch (NullPointerException e){
+            List<Post> aux = new ArrayList<>(posts);
+
+            lv = findViewById(R.id.listViewAll);
+            adapter = new PostListSearchAdapter(this, posts, aux);
+        }
+
 
         lv.setAdapter(adapter);
 
@@ -70,6 +103,49 @@ public class ShowAllPostsActivity extends BaseActivity implements SearchView.OnQ
             public void onClick(View v) {
                 Intent intent = new Intent(ShowAllPostsActivity.this, ShowMyPostsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        filter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(ShowAllPostsActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(ShowAllPostsActivity.this);
+                View view1 = inflater.inflate(R.layout.post_custom_dialog_filter_theme, null);
+                ListView listView = view1.findViewById(R.id.listView);
+                SearchView searchView = view1.findViewById(R.id.searchView);
+
+                final String[] arraySpinner = new String[] {
+                        "None", "General", "Administration", "Stories", "Tutorial", "Information", "Other"
+                };
+
+                final ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<>(ShowAllPostsActivity.this, android.R.layout.simple_list_item_1, arraySpinner);
+                listView.setAdapter(stringArrayAdapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent = new Intent(ShowAllPostsActivity.this, ShowAllPostsActivity.class);
+                        intent.putExtra("theme_filter", arraySpinner[position]);
+                        startActivity(intent);
+                    }
+                });
+
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String newText) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        stringArrayAdapter.getFilter().filter(newText);
+                        return false;
+                    }
+
+                });
+                dialog.setContentView(view1);
+                dialog.show();
             }
         });
 
